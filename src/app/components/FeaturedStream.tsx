3e135dbcb2e232d72a5a234a8db209226108b843
Patrick Obermueller <patrick.obermueller@willhaben.at>
2020-10-25 13:39:10 +0100

    [WIP] responsive featured twitch embedded stream

--- src/app/components/FeaturedStream.tsx
+++ src/app/components/FeaturedStream.tsx
@@ -1,7 +1,28 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import { StyledFeatured } from '../../styles/common.styles'
 
 const FeaturedStream: React.FunctionComponent = () => {
+	const featuredStreamRef = useRef(null)
+	const [size, setSize] = useState([0, 0])
+
+	useLayoutEffect(() => {
+		function updateSize() {
+			setSize([window.innerWidth, window.innerHeight])
+			if (featuredStreamRef.current?.children[0]) {
+				const el: HTMLIFrameElement = featuredStreamRef.current?.children[0] // twitch injected iframe stream window
+				let newWidth = window.innerWidth
+				if (newWidth >= 769) {
+					newWidth = window.innerWidth / 2
+				}
+				el.width = `${newWidth}px`
+				el.height = `${newWidth * (9 / 16)}px`
+			}
+		}
+		window.addEventListener('resize', updateSize)
+		updateSize()
+		return () => window.removeEventListener('resize', updateSize)
+	}, [])
+
 	useEffect(() => {
 		new Twitch.Embed('twitch-embed', {
 			width: 854,
@@ -11,7 +32,7 @@ const FeaturedStream: React.FunctionComponent = () => {
 		})
 	}, [])
 
-	return <StyledFeatured id="twitch-embed"></StyledFeatured>
+	return <StyledFeatured ref={featuredStreamRef} id="twitch-embed"></StyledFeatured>
 }
 
 export default FeaturedStream
