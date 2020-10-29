9b3886060c580707551c2f09f44ef80f79a38b74
Patrick Obermueller <patrick.obermueller@willhaben.at>
2020-10-29 18:45:44 +0100

    fix https://err.sh/next.js/no-document-title by ssr next head to page

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -45,7 +45,6 @@ export default class MyDocument extends Document {
 		return (
 			<Html lang="de">
 				<Head>
-					<title>Charity Royale 2020</title>
 					<link
 						href={`https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=${fontDisplay}`}
 						rel="stylesheet"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,17 @@ import React from 'react'
 import App, { InitialAppProps } from '../app/App'
 import { GetStaticProps, NextPage } from 'next'
 import cmsContent from '../../_posts/frontpage/charity-royale.md'
+import Head from 'next/head'
 
 const IndexPage: NextPage<InitialAppProps> = (props: InitialAppProps) => {
-	return <App {...props} />
+	return (
+		<>
+			<Head>
+				<title>Charity Royale 2020</title>
+			</Head>
+			<App {...props} />
+		</>
+	)
 }
 
 export const getStaticProps: GetStaticProps<InitialAppProps> = async () => {
