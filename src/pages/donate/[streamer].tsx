011bb8f149bd6cf4546f6101ecd14475fd3dc5e1
David Fasching <davidfasching@mailbox.org>
2020-11-07 23:55:46 +0100

    Apply donation layout to dynamic pages

--- src/pages/donate/[streamer].tsx
+++ src/pages/donate/[streamer].tsx
@@ -3,6 +3,18 @@ import { GetStaticPaths, GetStaticProps, NextPage } from 'next'
 import { useRouter } from 'next/router'
 import rawCmsContent from '../../../_posts/frontpage/charity-royale.md'
 import Head from 'next/head'
+import DonationHeader from '../../app/components/DonationHeader'
+import DonationWidget from '../../app/components/DonationWidget'
+import DonationWidgetCount from '../../app/components/DonationWidgetCount'
+import DonationWidgetList from '../../app/components/DonatorsWidgetList'
+import {
+	StyledDonationFormIframe,
+	StyledDonationSumWidget,
+	StyledDonatorsWidget,
+	StyledLatestDonatorssWidget,
+} from '../../styles/common.styles'
+import DonationLayout from '../../app/layouts/DonationLayout'
+import PageWithLayoutType from '../../app/types/PageWithLayout'
 
 interface InitialDonationProps {
 	project: Upcoming
@@ -13,6 +25,7 @@ interface Upcoming {
 	streamerName: string
 	streamerChannel: string
 	streamLink: string
+	makeAWishProjectId: string
 	descripion: string
 	imgUrl: string
 }
@@ -27,6 +40,62 @@ interface CmsContent {
 
 const cmsContent = rawCmsContent.attributes as CmsContent
 
+const highestDonatorsList = [
+	{
+		col_1: '1',
+		col_2: 'Philos',
+		col_3: '3.184,06',
+	},
+	{
+		col_1: '2',
+		col_2: 'FrufruTv',
+		col_3: '1.760,56',
+	},
+	{
+		col_1: '3',
+		col_2: 'swissduayne1996',
+		col_3: '1.419,97',
+	},
+	{
+		col_1: '4',
+		col_2: 'FreeSteyler01',
+		col_3: '862,92',
+	},
+	{
+		col_1: '5',
+		col_2: 'Darina420',
+		col_3: '530,12',
+	},
+]
+
+const latestDonators = [
+	{
+		col_1: 'Mo 21:02',
+		col_2: 'Philos',
+		col_3: '103,88',
+	},
+	{
+		col_1: 'Mo 09:53',
+		col_2: 'FrufruTv',
+		col_3: '50,00',
+	},
+	{
+		col_1: 'So 19:23',
+		col_2: 'swissduayne1996',
+		col_3: '60,00',
+	},
+	{
+		col_1: 'So 19:21',
+		col_2: 'FreeSteyler01',
+		col_3: '31,42',
+	},
+	{
+		col_1: 'So 19:15',
+		col_2: 'Darina420',
+		col_3: '103,88',
+	},
+]
+
 const DonatePage: NextPage<InitialDonationProps> = ({ project }: InitialDonationProps) => {
 	const router = useRouter()
 	const { streamer } = router.query
@@ -35,9 +104,29 @@ const DonatePage: NextPage<InitialDonationProps> = ({ project }: InitialDonation
 			<Head>
 				<title>Charity Royale 2020 - Spenden</title>
 			</Head>
-			<p>
-				Donate for {streamer}. {project.streamLink}
-			</p>
+
+			<DonationHeader title={`Spendenprojekt: ${project.streamerName}`} description={project.descripion} />
+
+			<StyledDonationFormIframe
+				src={`https://streamer.make-a-wish.at/charityroyale2020/donate/${project.makeAWishProjectId}`}
+				title="Spendenformular"
+			/>
+
+			<StyledDonationSumWidget>
+				<DonationWidget title={'Spendensumme'}>
+					<DonationWidgetCount current_amount={30} donation_goal_amount={100} />
+				</DonationWidget>
+			</StyledDonationSumWidget>
+			<StyledDonatorsWidget>
+				<DonationWidget title={'TOP-Spender'}>
+					<DonationWidgetList list={highestDonatorsList} />
+				</DonationWidget>
+			</StyledDonatorsWidget>
+			<StyledLatestDonatorssWidget>
+				<DonationWidget title={'Letzte Spender'}>
+					<DonationWidgetList list={latestDonators} />
+				</DonationWidget>
+			</StyledLatestDonatorssWidget>
 		</>
 	)
 }
@@ -63,5 +152,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
 		fallback: false,
 	}
 }
+;((DonatePage as unknown) as PageWithLayoutType).layout = DonationLayout
 
 export default DonatePage
