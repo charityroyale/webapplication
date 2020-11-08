e574681a225940fbd38eef7ceb696442c631b52e
David Fasching <davidfasching@mailbox.org>
2020-11-08 21:45:27 +0100

    Update MakeAWish DTO

--- src/app/dto/MakeAWishDonationsDTO.ts
+++ src/app/dto/MakeAWishDonationsDTO.ts
@@ -1,21 +1,26 @@
 export class MakeWishDonationsDTO {
 	public total_donation_sum = ''
 	public total_donation_count = -1
-	public projecs = { '': MakeWishDonationProjectDTO }
+	public projects = {} as { [projectId: string]: MakeWishDonationProjectDTO }
 }
 
-export class MakeAWishDonatorDTO {
+export class MakeAWishRecentDonatorDTO {
 	public unix_timestamp = -1
 	public name = ''
 	public amount = ''
 }
 
+export class MakeAWishTopDonatorDTO {
+	public name = ''
+	public amount = ''
+}
+
 export class MakeWishDonationProjectDTO {
-	public project_id = -1
+	public project_id = ''
 	public slug = ''
 	public donation_goal = ''
 	public current_donation_sum = ''
-	public current_donation_count = ''
-	public top_donators = [] as MakeAWishDonatorDTO[]
-	public recent_donators = [] as MakeAWishDonatorDTO[]
+	public current_donation_count = -1
+	public top_donators = [] as MakeAWishTopDonatorDTO[]
+	public recent_donators = [] as MakeAWishRecentDonatorDTO[]
 }
