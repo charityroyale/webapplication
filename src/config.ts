046f7d6d30057f1371ad1864efb044297bcd4d5c
David Fasching <davidfasching@mailbox.org>
2020-11-10 19:47:11 +0100

    Switch makeAWishAPI fetcher to live data and add refresh interval

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export const makeAWishAPI = {
 	donationsURL: 'https://streamer.make-a-wish.at/charityroyale2020/info.json',
 	donationFormURL: 'https://streamer.make-a-wish.at/charityroyale2020/donate/',
+	refreshInterval: 30000,
 }
