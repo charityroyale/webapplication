794ec34cd7c06362ae29af418d211d02c532fb79
lanort <github_com.20.Taube@spamgourmet.com>
2020-11-29 21:55:36 +0100

    Preload iframe content for donations
    Add <link rel="preload" for iframe page as well as Paypal SDK to increase loading speed.

--- src/pages/donate/[streamer].tsx
+++ src/pages/donate/[streamer].tsx
@@ -188,6 +188,8 @@ const DonatePage: NextPage<InitialDonationProps> = ({ project }: InitialDonation
 		<>
 			<Head>
 				<title>Charity Royal 2020 - {project.streamerName}</title>
+				<link rel="preload" href="${makeAWishAPI.donationFormURL}${project.makeAWishProjectId}">
+				<link rel="preload" href="https://www.paypal.com/sdk/js?client-id=ATL90eAe-2zLX8e1YYlyI_O6-gd_6qs9gBTimJ7hNiOa1ZUKNNs0XBy1MuF2vb_tY709L13K9akFjhWs&currency=EUR">
 			</Head>
 
 			<DonationHeader
