2c07694809a3074c30d0b4327bcabe56f67662f8
Patrick Obermueller <patrick.obermueller@willhaben.at>
2021-11-23 20:04:36 +0100

    update make-a-wish urls to 2021 and cookie banner  text

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const makeAWishAPI = {
-	donationsURL: 'https://streamer.make-a-wish.at/charityroyale2020/info.json',
-	donationFormURL: 'https://streamer.make-a-wish.at/charityroyale2020/donate/',
+	donationsURL: 'https://streamer.make-a-wish.at/charityroyale2021/info.json',
+	donationFormURL: 'https://streamer.make-a-wish.at/charityroyale2021/donate/',
 	refreshInterval: 30000,
 }
