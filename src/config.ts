e74fa5f689b1f648551f7a29bb543d9178041732
Patrick Obermueller <patrick.obermueller@willhaben.at>
2022-08-29 14:35:08 +0200

    cr2022: update donationforms to 2022

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const makeAWishAPI = {
 	donationsURL: 'https://streamer.make-a-wish.at/charityroyale2021/info.json',
-	donationFormURL: 'https://streamer.make-a-wish.at/charityroyale2021/donate/',
-	donationFormEnURL: 'https://streamer.make-a-wish.at/charityroyale2021/donate-en/',
+	donationFormURL: 'https://streamer.make-a-wish.at/charityroyale2022/donate/',
+	donationFormEnURL: 'https://streamer.make-a-wish.at/charityroyale2022/donate-en/',
 
 	refreshInterval: 30000,
 }
