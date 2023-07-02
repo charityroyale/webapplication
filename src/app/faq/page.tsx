27655fb09f48042342345c4d87bc426953cd9cab
Patrick Obermueller <pat.obermueller@gmail.com>
2023-07-02 22:09:45 +0200

    tech: move faq and halloffame metadata to appdir router

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,37 +1,34 @@
-import Head from 'next/head'
 import React from 'react'
 import { FaqContent } from './components/content'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	metadataBase: new URL('https://charityroyale.at'),
+	title: `Charity Royale | FAQ`,
+	openGraph: {
+		title: `Charity Royale | FAQ`,
+		description: 'Größtes Stream,- und Gaming Charity Projekt Österreichs von Veni und willhaben.',
+		url: '/',
+		siteName: `Charity Royale | FAQ`,
+		images: [
+			{
+				url: '/uploads/charity_royale_rgb_300x300.png',
+				width: 300,
+				height: 300,
+			},
+		],
+		locale: 'de_AT',
+		type: 'website',
+	},
+	twitter: {
+		title: `Charity Royale`,
+		creator: '@CharityRoyale',
+		description: `Größtes Stream,- und Gaming Charity Projekt Österreichs von Veni und willhaben.`,
+		images: ['/uploads/charity_royale_rgb_300x300.png'],
+	},
+}
 
 const FaqPage = () => {
-	return (
-		<>
-			<Head>
-				<title>Charity Royale - FAQ</title>
-				<meta name="twitter:card" content="summary" key="twcard" />
-				<meta name="twitter:site" content={'@CharityRoyale'} key="twsite" />
-				<meta name="twitter:creator" content={'@CharityRoyale'} key="twcreator" />
-
-				<meta property="og:url" content={'https://charityroyale.at/'} key="ogurl" />
-				<meta
-					property="og:image"
-					content={'https://charityroyale.at/uploads/charity_royale_rgb_300x300.png'}
-					key="ogimage"
-				/>
-				<meta property="og:image:width" content={'300'} key="ogimagewidth" />
-				<meta property="og:image:height" content={'300'} key="ogimageheight" />
-				<meta property="og:site_name" content={'Charity Royale'} key="ogsitename" />
-				<meta property="og:title" content={'Charity Royale | FAQ'} key="ogtitle" />
-				<meta property="og:type" content={'website'} key="ogtype" />
-				<meta property="og:locale" content={'de_AT'} key="oglocale" />
-				<meta property="fb:app_id" content={process.env.FB_ID} key="fbappid" />
-				<meta
-					property="og:description"
-					content={'Größtes Stream,- und Gaming Charity Projekt Österreichs von Veni und willhaben.'}
-					key="ogdesc"
-				/>
-			</Head>
-			<FaqContent />
-		</>
-	)
+	return <FaqContent />
 }
 export default FaqPage
--- src/app/halloffame/page.tsx
+++ src/app/halloffame/page.tsx
@@ -1,43 +1,40 @@
-import Head from 'next/head'
 import React from 'react'
 import { DonorsResponse } from '../hooks/useDonors'
 import { makeAWishAPI } from '../../config'
 import { HallOfFameContent } from './components/content'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+	metadataBase: new URL('https://charityroyale.at'),
+	title: `Charity Royale | Hall Of Fame`,
+	openGraph: {
+		title: `Charity Royale | Hall Of Fame`,
+		description: 'Größtes Stream,- und Gaming Charity Projekt Österreichs von Veni und willhaben.',
+		url: '/',
+		siteName: `Charity Royale | Hall Of Fame`,
+		images: [
+			{
+				url: '/uploads/charity_royale_rgb_300x300.png',
+				width: 300,
+				height: 300,
+			},
+		],
+		locale: 'de_AT',
+		type: 'website',
+	},
+	twitter: {
+		title: `Charity Royale`,
+		creator: '@CharityRoyale',
+		description: `Größtes Stream,- und Gaming Charity Projekt Österreichs von Veni und willhaben.`,
+		images: ['/uploads/charity_royale_rgb_300x300.png'],
+	},
+}
 
 const Page = async () => {
 	const res = await fetch(makeAWishAPI.donorsURL)
 	const donors = ((await res.json()) as DonorsResponse).donors
 
-	return (
-		<>
-			<Head>
-				<title>Charity Royale | Hall Of Fame</title>
-				<meta name="twitter:card" content="summary" key="twcard" />
-				<meta name="twitter:site" content={'@CharityRoyale'} key="twsite" />
-				<meta name="twitter:creator" content={'@CharityRoyale'} key="twcreator" />
-
-				<meta property="og:url" content={'https://charityroyale.at/'} key="ogurl" />
-				<meta
-					property="og:image"
-					content={'https://charityroyale.at/uploads/charity_royale_rgb_300x300.png'}
-					key="ogimage"
-				/>
-				<meta property="og:image:width" content={'300'} key="ogimagewidth" />
-				<meta property="og:image:height" content={'300'} key="ogimageheight" />
-				<meta property="og:site_name" content={'Charity Royale'} key="ogsitename" />
-				<meta property="og:title" content={'Charity Royale | Hall Of Fame'} key="ogtitle" />
-				<meta property="og:type" content={'website'} key="ogtype" />
-				<meta property="og:locale" content={'de_AT'} key="oglocale" />
-				<meta property="fb:app_id" content={process.env.FB_ID} key="fbappid" />
-				<meta
-					property="og:description"
-					content={'Größtes Stream,- und Gaming Charity Projekt Österreichs von Veni und willhaben.'}
-					key="ogdesc"
-				/>
-			</Head>
-			<HallOfFameContent donors={donors} />
-		</>
-	)
+	return <HallOfFameContent donors={donors} />
 }
 
 export default Page
