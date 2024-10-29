090c638ade164d3890b5361dc1147287580c8831
Patrick Obermueller <pat.obermueller@gmail.com>
2024-10-29 14:29:34 +0100

    improve error and 404

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,16 +1,41 @@
 'use client'
 
 import React, { useEffect } from 'react'
+import styled from 'styled-components'
 
-export default function Error({ error, reset }: { error: Error; reset: () => void }) {
+export default function Error({ error }: { error: Error; reset: () => void }) {
 	useEffect(() => {
 		console.error(error)
 	}, [error])
 
 	return (
-		<div>
-			<h2>Do hots wos!</h2>
-			<button onClick={() => reset()}>Rerender</button>
-		</div>
+		<ErrorWraper>
+			<ErrorTitle>Fehler Royale: Do hots wos :-(</ErrorTitle>
+			<ErrorDescription>
+				Leider ist ein Problem aufgetreten. Bitte lade die Seite in ein paar Minuten nocheinmal. Sollte das
+				Problem weiterbestehen melde dich bitte bei uns.
+			</ErrorDescription>
+		</ErrorWraper>
 	)
 }
+
+const ErrorTitle = styled.h2`
+	color: ${(p) => p.theme.color.white};
+	text-align: center;
+	font-size: ${(p) => p.theme.fontSize.xl}px;
+`
+const ErrorDescription = styled.p`
+	color: ${(p) => p.theme.color.white};
+	text-align: center;
+	font-size: ${(p) => p.theme.fontSize.l}px;
+`
+
+const ErrorWraper = styled.div`
+	height: 100vh;
+	display: flex;
+	padding: 24px 24px;
+	align-items: center;
+	gap: 12px;
+	justify-content: center;
+	flex-direction: column;
+`
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,36 @@
+'use client'
+
 import React from 'react'
+import { styled } from 'styled-components'
 
 export default function NotFound() {
-	return <h2>Seite nicht gefunden</h2>
+	return (
+		<ErrorWraper>
+			<ErrorTitle>Diese Seite gibt es nicht</ErrorTitle>
+			<ErrorDescription>
+				Hast du dich verirrt? <a href="/">Hier gehts zur Hauptseite</a>
+			</ErrorDescription>
+		</ErrorWraper>
+	)
 }
+
+const ErrorTitle = styled.h2`
+	color: ${(p) => p.theme.color.white};
+	text-align: center;
+	font-size: ${(p) => p.theme.fontSize.xl}px;
+`
+const ErrorDescription = styled.p`
+	color: ${(p) => p.theme.color.white};
+	text-align: center;
+	font-size: ${(p) => p.theme.fontSize.l}px;
+`
+
+const ErrorWraper = styled.div`
+	height: 100vh;
+	display: flex;
+	padding: 24px 24px;
+	align-items: center;
+	gap: 12px;
+	justify-content: center;
+	flex-direction: column;
+`
