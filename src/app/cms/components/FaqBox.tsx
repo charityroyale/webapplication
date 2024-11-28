eda296682837d33188a712f3f936552d6c48bae4
Patrick Obermueller <pat.obermueller@gmail.com>
2024-11-28 17:39:23 +0100

    donationGoalCall

--- src/app/cms/components/FaqBox.tsx
+++ src/app/cms/components/FaqBox.tsx
@@ -126,7 +126,7 @@ const GoalTitle = styled.p`
 const ButtonsBox: React.FunctionComponent = () => {
 	return (
 		<ButtonsGrid>
-			<GoalTitle>Road to 1 Million Euro</GoalTitle>
+			<GoalTitle>Road to 500.000 €</GoalTitle>
 			<GoalText>
 				<Text content="donationGoalCall" />
 			</GoalText>
